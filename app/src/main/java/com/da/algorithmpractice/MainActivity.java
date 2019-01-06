85bf3e8ae815fd6285a831e505c7dc8b118868ca
dajeongpark <ekwjd37891@naver.com>
2019-01-06 15:33:47 +0900

    Initial commit

---
+++ app/src/main/java/com/da/algorithmpractice/MainActivity.java
@@ -0,0 +1,31 @@
+package com.da.algorithmpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
